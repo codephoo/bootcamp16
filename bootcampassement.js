7f76f487b973b944e73fca3882c21d8c931f3b11
Shola <admin@codedgeek.com>
2016-05-18 04:19:06 +0100

    Added validations and two functionalites

--- bootcampassement.js
+++ bootcampassement.js
@@ -1,12 +1,19 @@
 function NotesApplication(author) {
 	'use strict';
-	this.author=author ;
-
+	this.author=verifyAuthor(author) ;
 	this.notes=[];
 	this.create= function(note_content){
 		this.notes.push(note_content);
 
 	};
+	function verifyAuthor(author){
+		if (typeof author == typeof '' && author !=''){
+			return author;
+		}
+		else {
+			throw new TypeError('Invalid Author');
+		}
+	}
 	this.listnodes= function() {
 		var allnotes='';
 		for(var i=0;i<this.notes.length;i++){
@@ -15,20 +22,44 @@ function NotesApplication(author) {
 		return allnotes;
 	};
 	this.get= function(note_id) {
-		if(typeof note_id==Number) return this.notes[note_id];
+		if(typeof note_id==typeof 1) {
+			return this.notes[note_id];
+		}
 		else{
 			return "Invalid note id";
 		}
 	};
 	this.search= function(search_text) {
+		search_text=String(search_text);
 		var matches=[];
 		for(var i in this.notes){
 			if(this.notes[i].indexOf(search_text)>-1){
-				matches.push('Showing result for search ' + search_text +' Note_ID: ' + i + "\n" + this.notes[i] + '\n' + "By Author " + this.author );
+				matches.push('Showing result for search \'' + search_text +'\' Note_ID: ' + i + "\n" + this.notes[i] + '\n' + "By Author " + this.author );
 			}
 		}
-		return matches.join('\n');
+		return matches.length>0?matches.join('\n'):'No match found';
 		
+	};
+	this.delete = function(note_id) {
+		if(note_id>=this.notes.length || typeof note_id != typeof 1){
+			throw new Error("note_id must be a number less than "+ this.notes.length);
+		}
+		else{
+			delete this.notes[note_id];
+		}
+		return 'Note deleted';
+	};
+	this.edit = function(note_id,new_content){
+		if(note_id>=this.notes.length || typeof note_id != typeof 1){
+			throw new Error("note_id must be a number less than "+ this.notes.length);
+		}
+		else if(new_content=='' || typeof new_content != typeof ''){
+			throw new Error("new_content must be a non-empty string");
+		}
+		else{
+			this.notes[note_id]= new_content;
+		}
+		return "Content edited";
 	};
 
 }
