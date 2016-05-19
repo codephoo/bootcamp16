b2484c904a0688bb5caf661f4ddb670bac7b5066
Shola <admin@codedgeek.com>
2016-05-19 06:56:57 +0100

    Creat suite and spec for get method of NoteApplication

--- Jasmine/bootcamp.spec.js
+++ Jasmine/bootcamp.spec.js
@@ -1,5 +1,5 @@
 describe("list notes", function() {
-    it(" function", function() {
+    it(" should be empty", function() {
         var note = new NotesApplication('shola');
         expect(note.listNotes()).toBe('');
   });
@@ -8,7 +8,7 @@ describe("list notes", function() {
         note.create('');
         expect(note.listNotes()).toBe('');
   });
-    it("should be empty and of type string", function() {
+    it(" should be empty and of type string", function() {
         var note = new NotesApplication('shola');
         expect(note.listNotes()).toBe('' && typeof '');
 
@@ -18,9 +18,28 @@ describe("list notes", function() {
         note.create(3);
         expect(note.listNotes()).toBe('' && typeof '');
     })
-    it("should be empty and of type string", function() {
-        var note = new NotesApplication('shola');
-        note.create(true);
-        expect(note.listNotes()).toBe('' && typeof '');
-    })
 });
+
+describe("Get a note with a specific id", function() {
+    it("should be undefined", function() {
+        var note = new NotesApplication('Alex');
+        expect(note.get(0)).toBe(undefined);
+  });
+    it("should be the same as created note", function() {
+        var note = new NotesApplication('shola');
+        note.create('My name is Shola Abaogun');
+        expect(note.get(0)).toBe('My name is Shola Abaogun');
+  });
+    it("should be the same as created note", function() {
+        var note = new NotesApplication('shola');
+        note.create('My name is Shola Abaogun');
+        expect(typeof note.get(0)).toBe(typeof '');
+  });
+    it("should return index out of range", function() {
+        var note = new NotesApplication('shola');
+        note.create('My name is Shola Abaogun');
+        expect(note.get(1)).toBe('Index out of range');
+  });
+
+});
+  
